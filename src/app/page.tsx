dce18d045683f97aa7ab041ee8bb83676c74fcc5
NeilRow56 <neilrowland56@gmail.com>
2025-10-22 17:15:53 +0100

    :added advanced forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import AdvancedFullForm from '@/components/forms/advanced-full-form'
 import { BugReportForm } from '@/components/forms/bug-report-form'
 
 export default function Home() {
   return (
     <div className='container mx-auto flex w-full'>
-      <div className='mx-auto mt-48 flex sm:min-w-md'>
-        <BugReportForm />
+      <div className='mx-auto mt-8 flex flex-col space-y-8 sm:min-w-md'>
+        {/* <BugReportForm /> */}
+        <AdvancedFullForm />
       </div>
     </div>
   )
